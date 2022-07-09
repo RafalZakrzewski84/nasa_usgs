3185a88ae09da1d012ef75637467d10e929f9bab
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-09 15:09:43 +0200

    Add fetching form NASA APOD API

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,11 +1,34 @@
 /** @format */
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import APIs from '../helpers/APIs';
 
 function Homepage() {
+	const [apod, setAPOD] = useState([]);
+	useEffect(() => {
+		console.log('before axios');
+
+		const NASA_API = `${APIs.nasa.base_url}${APIs.nasa.API_KEY}`;
+		console.log(NASA_API);
+
+		axios
+			.get(NASA_API)
+			.then((response) => {
+				//printing axios response
+				console.log(response);
+
+				//setting axios response to todaysArticles
+				setAPOD(response.data.data);
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	}, []);
+
 	return (
 		<React.Fragment>
 			<CssBaseline />
--- src/components/helpers/APIs.js
+++ src/components/helpers/APIs.js
@@ -1,7 +1,10 @@
 /** @format */
 
 const APIs = {
-	NASA_API_KEY: 'doeIdytEkIUcxDtoR2ZHh2myFh2e7G1vWRwIMJh5',
+	nasa: {
+		base_url: 'https://api.nasa.gov/planetary/apod?api_key=',
+		API_KEY: 'doeIdytEkIUcxDtoR2ZHh2myFh2e7G1vWRwIMJh5',
+	},
 };
 
 export default APIs;
