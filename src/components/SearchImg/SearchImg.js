a20871f028b13c4d22f33377ad1dd70579dcd12e
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-07-09 14:45:44 +0200

    adding containers and resetting links decoration

--- src/components/SearchImg/SearchImg.js
+++ src/components/SearchImg/SearchImg.js
@@ -1,12 +1,18 @@
 /** @format */
 
 import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 function SearchImg() {
 	return (
-		<div>
-			<h1>SearchImg</h1>
-		</div>
+		<React.Fragment>
+			<CssBaseline />
+			<Container maxWidth="xl">
+				<Box sx={{ bgcolor: '#cfe8fc', height: '100vh', my: '1rem' }} />
+			</Container>
+		</React.Fragment>
 	);
 }
 
